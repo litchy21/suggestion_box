fdc9ec7872d16312c1e221657f98aa26df949595
Louise <louise.roussel@epitech.eu>
2018-07-17 16:50:22 +0200

    Add TextType

--- src/Form/IdeaType.php
+++ src/Form/IdeaType.php
@@ -6,13 +6,14 @@ use App\Entity\Idea;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class IdeaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', null, array('label' => 'Add an idea'))
+            ->add('name', TextType::class, array('label' => 'Add an idea'))
         ;
     }
 
